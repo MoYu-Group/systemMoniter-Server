6b63acae5f8c205e257f134ad8e894d9a0bd4c2f
CHN-STUDENT <CHN-STUDENT@outlook.com>
2022-07-13 15:53:18 +0800

    Update mysql.go

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -17,7 +17,7 @@ var db *gorm.DB
 var sqlDB *sql.DB
 
 func Init() (err error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		viper.GetString("mysql.user"),
 		viper.GetString("mysql.password"),
 		viper.GetString("mysql.host"),
@@ -172,6 +172,6 @@ func FindNodeIDByNameAndHost(name string, host string) (error, string) {
 
 func FindAllNodeStatus() (error, []models.Status) {
 	var allNodeStatus []models.Status
-	result := db.Raw("SELECT n.name,n.type,n.host,n.location,n.disabled, i.* FROM `nodes` as n left JOIN `infos` as i on n.id = i.node_id ORDER BY lastest_time DESC").Scan(&allNodeStatus)
+	result := db.Raw("SELECT n.name,n.type,n.host,n.location,n.disabled, i.* FROM `nodes` as n left JOIN `infos` as i on n.id = i.node_id WHERE i.is_latest = true ORDER BY lastest_time DESC").Scan(&allNodeStatus)
 	return result.Error, allNodeStatus
 }
